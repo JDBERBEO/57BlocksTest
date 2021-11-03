21e75d00b633805e7e70a860ae1cd4c6e57bffe2
jd.berbeo <jd.berbeo@gmail.com>
2021-11-02 20:41:50 -0500

    renders details card

--- src/components/views/pokemons/PokemonView.js
+++ src/components/views/pokemons/PokemonView.js
@@ -1,15 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './pokemonView.css';
 
 export const PokemonView = ({ pokemon }) => {
+  console.log('pokemon: ', pokemon);
   return (
     <div>
       {!!pokemon && (
         <>
-          <img src={pokemon.sprites.front_shiny} alt="pokemon description" />
-          <h1>name: {pokemon.name}</h1>
-          <p>weight: {pokemon.weight}</p>
-          <Link to="/">go back</Link>
+          <div className="pokemonCardDetails">
+            <h1>{pokemon.name}</h1>
+            <img
+              src={pokemon.sprites.front_shiny}
+              className="cardImgTop"
+              alt="pokemon img"
+            />
+            <div className="cardBody">
+              <p className="cardDetail">
+                Base experience: {pokemon.base_experience}
+              </p>
+              <p className="cardDetail">Weight: {pokemon.weight}</p>
+              <p className="cardDetail">Height: {pokemon.height}</p>
+              <p className="cardDetail">
+                Abilities:{' '}
+                <ul>
+                  {pokemon.abilities.map((ability) => (
+                    <React.Fragment key={Math.random()}>
+                      <li className="ability">{ability.ability.name}</li>
+                    </React.Fragment>
+                  ))}
+                </ul>
+              </p>
+              <Link to="/">go back</Link>
+            </div>
+          </div>
         </>
       )}
     </div>
